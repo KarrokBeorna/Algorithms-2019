c3777ee95214cade0ebdaf41439f9af8568f5b9f
KarrokBeorna <rfyjybh2000@mail.ru>
2019-10-22 18:44:56 +0300

    1/2 - "The end..."

--- test/lesson2/AbstractAlgorithmsTests.kt
+++ test/lesson2/AbstractAlgorithmsTests.kt
@@ -46,6 +46,7 @@ abstract class AbstractAlgorithmsTests {
         assertEquals(3 to 4, optimizeBuyAndSell("input/buysell_in1.txt"))
         assertEquals(8 to 12, optimizeBuyAndSell("input/buysell_in2.txt"))
         assertEquals(3 to 4, optimizeBuyAndSell("input/buysell_in3.txt"))
+        assertEquals(3 to 5, optimizeBuyAndSell("input/buysell_in4.txt"))
         try {
             val expectedAnswer = generatePrices(1000)
             assertEquals(expectedAnswer, optimizeBuyAndSell("temp_prices.txt"))
